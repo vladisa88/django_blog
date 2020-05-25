c41eb36b8cf1522f92449043aeeb09e9b0d1c0be
vladisa88 <vladisa88@gmail.com>
2020-05-26 00:45:25 +0400

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>', views.PostDetail.as_view(), name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+]
